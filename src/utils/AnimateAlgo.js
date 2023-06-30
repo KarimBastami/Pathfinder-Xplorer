9eb80f657640c3d1a405b40ba667990eebbde980
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2023-06-30 16:51:10 +0300

    fix: Run Btn Enabled Before Shortest Path Animation Is FInished

--- src/utils/AnimateAlgo.js
+++ src/utils/AnimateAlgo.js
@@ -30,26 +30,29 @@ const animateShortestPath = async (nodesInShortestPath, refArray) => {
   return new Promise((resolve, reject) => {
     const animationDuration = 30
 
-    nodesInShortestPath.forEach((node, i) => {
-      if (i == nodesInShortestPath.length - 1){
-        setTimeout(() => {
-          resolve()
-        }, animationDuration * i);
-      }
-      
-      setTimeout(() => {
-        if (!node.isStart && !node.isTarget) {
-          refArray.forEach((ref) => {
-            const refNode = ref.current
-  
-            if (refNode.getAttribute("id") === `${node.y}-${node.x}`) {
-              refNode.classList.add("shortest-path")
-            }
-          })
+    if (nodesInShortestPath.length) {
+      nodesInShortestPath.forEach((node, i) => {
+        if (i == nodesInShortestPath.length - 1){
+          setTimeout(() => {
+            resolve()
+          }, animationDuration * i);
         }
-      }, animationDuration * i);
-    })
-
-    resolve()
+        
+        setTimeout(() => {
+          if (!node.isStart && !node.isTarget) {
+            refArray.forEach((ref) => {
+              const refNode = ref.current
+    
+              if (refNode.getAttribute("id") === `${node.y}-${node.x}`) {
+                refNode.classList.add("shortest-path")
+              }
+            })
+          }
+        }, animationDuration * i);
+      })
+    }
+    else {
+      resolve()
+    }
   })
 }
