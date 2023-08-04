286818b007a9374b62a830b9dc5eef171d909ae9
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2023-08-04 21:42:30 +0300

    feat: Navigator to External Links

---
+++ src/components/external-links/ExternalLinks.jsx
@@ -0,0 +1,45 @@
+import "./ExternalLinks.css"
+
+import {
+  IconButton,
+  SpeedDial,
+  SpeedDialHandler,
+  SpeedDialContent,
+  SpeedDialAction,
+} from "@material-tailwind/react";
+
+import {
+  PlusIcon,
+  HomeIcon,
+  CodeBracketIcon,
+} from "@heroicons/react/24/outline";
+
+function ExternalLinks() {
+  return (
+    <div id="links">
+      <SpeedDial>
+          <SpeedDialHandler>
+            <IconButton size="lg" className="rounded-full bg-accent hover:shadow-none shadow-none">
+              <PlusIcon className="h-6 w-6 transition-transform group-hover:rotate-45" />
+            </IconButton>
+          </SpeedDialHandler>
+
+          <SpeedDialContent>
+            <SpeedDialAction className="h-14 w-14 shadow">
+              <a href="https://karimbastami.com" target="_blank" rel="noreferrer">
+                <HomeIcon className="h-6 w-6" />
+              </a>
+            </SpeedDialAction>
+
+            <SpeedDialAction className="h-14 w-14 shadow">
+              <a href="https://karimbastami.com" target="_blank" rel="noreferrer">
+                <CodeBracketIcon className="h-6 w-6" />
+              </a>
+            </SpeedDialAction>
+          </SpeedDialContent>
+        </SpeedDial>
+    </div> 
+  )
+}
+
+export default ExternalLinks
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../../components/navbar/Navbar'
 import Board from '../../components/grid/Board'
+import ExternalLinks from '../../components/external-links/ExternalLinks'
 
 function Home() {
   return (
     <>
       <Navbar />
       <Board />
+      <ExternalLinks />
     </>
   )
 }
