911a566a79b2c76538767cc12a36b99fa513c470
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2023-08-05 13:36:34 +0300

    feat: Mobile Cat Img Removed From Low Height Screens

--- src/views/mobile/Mobile.jsx
+++ src/views/mobile/Mobile.jsx
@@ -1,7 +1,13 @@
 import NavbarMobile from "../../components/navbar-mobile/NavbarMobile"
 import SadCat from "../../assets/img/sad-cat.png"
 
+import { useWindowSize } from '@uidotdev/usehooks'
+
 function Mobile() {
+
+  const windowSize = useWindowSize()
+  const showCatImg = windowSize.height > 600
+
   return (
     <>
       <NavbarMobile />
@@ -9,7 +15,8 @@ function Mobile() {
       <section id="mobile-showcase" className="px-6 py-4">
         <div className="container">
           <div className="h-75 flex flex-col text-center items-center justify-center">
-            <img width={120} height={120} src={SadCat} className="mb-2"/>
+            {showCatImg && <img width={120} height={120} src={SadCat} className="mb-2"/>}
+            
             <h3 className="text-h1 xs:text-chungus mb-8">Unavailable On Mobile & Tablet Devices</h3>
 
             <p>Due to the grid and the nature of the implemented drag mechanics, a mobile version of this app would not provide a seamless UX experience and therefore was disabled</p>
